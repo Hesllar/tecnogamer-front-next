2ff7e7d43ca83d13408bb5dfebb8e1f4c009ae71
Hesllar Henry <hesllar.linzmayer@gmail.com>
2025-03-11 21:36:24 -0300

    se añade nuva ruta productos, también se añaden nuevas imagenes par aquella página

--- public/index.ts
+++ public/index.ts
@@ -1,4 +1,11 @@
+//* Destacados
 export * as amdRyzen5800 from "./image/procesadores/AMD_RYZEN_7_5800X.webp";
 export * as amdRyzen9900 from "./image/procesadores/AMD_REYZEN_9_9900X.webp";
 export * as intelCore11400 from "./image/procesadores/INTEL_CORE_5_11400.webp";
 export * as intelCoreUltra265K from "./image/procesadores/INTEL_CORE_ULTRA_7_265K.webp";
+
+//* Productos
+export * as gabinete from "./image/productos/gabinete.webp";
+export * as gpu from "./image/productos/gpus.webp";
+export * as procesador from "./image/productos/procesadores.webp";
+export * as ramMemory from "./image/productos/ram_memory.webp";
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,52 @@
-import { Products } from "@/components/Products/Products";
+import Image from "next/image";
+import { gabinete, gpu, ramMemory, procesador } from "../../../public/index";
 
 export default function ProductPage() {
-  return <h1>Products</h1>;
+  return (
+    <div className="flex flex-col gap-8 items-center pt-8">
+      <h1 className="text-4xl font-bold text-center">Productos</h1>
+      <div className="grid grid-cols-1 2xl:grid-cols-2 min-[1921px]:grid-cols-4 gap-28 items-center">
+        <div className="shadow-2xl rounded-lg max-w-md items-center cursor-pointer">
+          <div className="w-full flex justify-center items-center">
+            <Image src={gabinete} alt="gabinete" className="w-full" />
+          </div>
+          <div className=" text-center">
+            <button className="w-full rounded-b-lg text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Ir a gabinetes
+            </button>
+          </div>
+        </div>
+        <div className="shadow-2xl rounded-lg max-w-md items-center cursor-pointer">
+          <div className="w-full flex justify-center items-center">
+            <Image src={gpu} alt="gpu" className="w-full" />
+          </div>
+          <div className=" text-center">
+            <button className="w-full rounded-b-lg text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Ir a tarjetas gráficas
+            </button>
+          </div>
+        </div>
+        <div className="shadow-2xl rounded-lg max-w-md items-center cursor-pointer">
+          <div className="w-full flex justify-center items-center">
+            <Image src={ramMemory} alt="ramMemory" className="w-full" />
+          </div>
+          <div className=" text-center">
+            <button className="w-full rounded-b-lg text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Ir a memorias RAM
+            </button>
+          </div>
+        </div>
+        <div className="shadow-2xl rounded-lg max-w-md items-center cursor-pointer">
+          <div className="w-full flex justify-center items-center">
+            <Image src={procesador} alt="procesador" className="w-full" />
+          </div>
+          <div className=" text-center">
+            <button className="w-full rounded-b-lg text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Ir a procesadores
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
