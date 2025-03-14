88953fe67f7fddade26c06ab82b56f313475648f
Hesllar Henry <hesllar.linzmayer@gmail.com>
2025-03-13 21:21:50 -0300

    se añade ruta y página referente a tarjetas de videos

---
+++ src/app/products/video-cards/page.tsx
@@ -0,0 +1,27 @@
+import dataJson from "../../../data/products.json";
+import { Products } from "@/components/Products/Products";
+import gpu1 from "../../../../public/image/tarjetasVideo/gpu1.webp";
+import gpu2 from "../../../../public/image/tarjetasVideo/gpu2.webp";
+import gpu3 from "../../../../public/image/tarjetasVideo/gpu3.webp";
+import gpu4 from "../../../../public/image/tarjetasVideo/gpu4.webp";
+import gpu5 from "../../../../public/image/tarjetasVideo/gpu5.webp";
+
+export default function VideoCardsPage() {
+  const imagesData: { [key: number]: any } = {
+    0: gpu1,
+    1: gpu2,
+    2: gpu3,
+    3: gpu4,
+    4: gpu5,
+  };
+  const videoCardsMapper = dataJson.videoCards.map((videoCard, index) => ({
+    ...videoCard,
+    image: imagesData[index],
+  }));
+  return (
+    <div className="flex flex-col items-center gap-8 pt-12 md:pt-8">
+      <h2 className="font-semibold text-3xl md:text-4xl">Tarjetas de video</h2>
+      <Products products={videoCardsMapper} />
+    </div>
+  );
+}
