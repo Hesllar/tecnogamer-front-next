6d1d68ecf30c26a370239ca1f325df181dbb4f59
Hesllar Henry <hesllar.linzmayer@gmail.com>
2025-06-08 22:24:58 -0400

    se añade nuevo servicio en productos

---
+++ src/app/api/products/sub-category/[subcategory-id]/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+interface Segments {
+  params: {
+    ["subcategory-id"]: number;
+  };
+}
+
+export async function GET(request: Request, { params }: Segments) {
+  const subCategoryId = params["subcategory-id"];
+
+  if (!subCategoryId || isNaN(Number(subCategoryId))) {
+    return NextResponse.json(
+      { message: "ID de categoría no válido" },
+      { status: 400 }
+    );
+  }
+
+  const product = await prisma.product.findMany({
+    where: {
+      category_id: +subCategoryId,
+    },
+  });
+
+  if (product.length === 0) {
+    return NextResponse.json(
+      { message: `No hay productos para esa categoría` },
+      { status: 404 }
+    );
+  }
+
+  const productsMapper = product.map((product) => ({
+    id: product.id,
+    name: product.name,
+    namenormalized: product.namenormalized,
+    price: product.price,
+    images: product.images,
+  }));
+  return NextResponse.json(productsMapper);
+}
